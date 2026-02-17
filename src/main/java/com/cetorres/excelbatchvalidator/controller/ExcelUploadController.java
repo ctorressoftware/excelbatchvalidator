5b8a3635c770a771161120c9c758080983f108a3
Carlos Torres <ctorressoftware@hotmail.com>
2026-02-17 11:13:59 -0400

    feat: set endpoint and add test logic

--- src/main/java/com/cetorres/excelbatchvalidator/controller/ExcelUploadController.java
+++ src/main/java/com/cetorres/excelbatchvalidator/controller/ExcelUploadController.java
@@ -1,4 +1,18 @@
 package com.cetorres.excelbatchvalidator.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("excel/")
 public class ExcelUploadController {
+
+    @PostMapping("upload")
+    public String uploadExcel(@RequestParam("file") MultipartFile file) {
+        return "Hola a todos";
+    }
+
 }
