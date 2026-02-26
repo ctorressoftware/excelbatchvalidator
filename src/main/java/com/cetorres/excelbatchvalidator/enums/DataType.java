76fc0aa1c1c405e2afba0029bc885a61fd5e19ba
Carlos Torres <ctorressoftware@hotmail.com>
2026-02-26 00:20:07 -0400

    feat: improve datatype enum

--- src/main/java/com/cetorres/excelbatchvalidator/enums/DataType.java
+++ src/main/java/com/cetorres/excelbatchvalidator/enums/DataType.java
@@ -2,13 +2,11 @@ package com.cetorres.excelbatchvalidator.enums;
 
 public enum DataType {
     DNI("dni"),
-    FIRST_NAME("firstName"),
-    SECOND_NAME("secondName"),
-    FIRST_LASTNAME("firstLastName"),
-    SECOND_LASTNAME("secondLastName"),
+    NAME("name"),
+    LASTNAME("lastname"),
     GENDER("gender"),
     EMAIL("email"),
-    PHONE_NUMBER("phoneNumber");
+    PHONE_NUMBER("phone_number");
 
     final private String name;
 
