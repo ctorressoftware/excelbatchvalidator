0f403b0174276dafc155d486cec74fca6c1b08ad
Carlos Torres <ctorressoftware@hotmail.com>
2026-02-27 01:04:11 -0400

    feat: add shared resource class named validationStats

--- src/main/java/com/cetorres/excelbatchvalidator/domain/ValidationStats.java
+++ src/main/java/com/cetorres/excelbatchvalidator/domain/ValidationStats.java
@@ -1,6 +1,38 @@
 package com.cetorres.excelbatchvalidator.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
 // TODO: create stats and metrics. Each worker has to write and sign this object
 public class ValidationStats {
+    private volatile AtomicBoolean claimed;
+    private volatile AtomicLong rowsCount;
+    private final List<String> signatures = Collections.synchronizedList(new ArrayList<>());
 
+    public void setClaimed(boolean expectedValue, boolean newValue) {
+        this.claimed.compareAndSet(expectedValue, newValue);
+    }
+
+    public boolean getClaimed() {
+        return claimed.get();
+    }
+
+    public void setRowsCount(long expectedValue, long newValue) {
+        this.rowsCount.compareAndSet(expectedValue, newValue);
+    }
+
+    public long getRowsCount() {
+        return rowsCount.get();
+    }
+
+    public void addSignature(String signature) {
+        this.signatures.add(signature);
+    }
+
+    public List<String> getSignatures() {
+        return Collections.unmodifiableList(signatures);
+    }
 }
